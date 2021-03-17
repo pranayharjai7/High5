9654587d5e11337465e39ae2b890eb9b48565df9
pranayharjai7 <pranayharjai7@gmail.com>
2021-03-17 23:55:57 +0100

    Dependencies Updated

--- src/main/java/Survey5/App.java
+++ src/main/java/Survey5/App.java
@@ -6,11 +6,10 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
-import java.util.concurrent.TimeUnit;
 
 public class App extends Application {
 
-    private static Scene scene,scene2;
+    private static Scene scene;
 
     @Override
     public void start(Stage stage) throws IOException {
