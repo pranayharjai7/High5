a1c0bdb2b0deb75259906fca2e5bac18f7cb7ea6
pranayharjai7 <pranayharjai7@gmail.com>
2021-03-25 22:11:36 +0100

    Logo Updated

--- src/main/java/Survey5/PrimaryController.java
+++ src/main/java/Survey5/PrimaryController.java
@@ -4,11 +4,15 @@ import java.io.IOException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 
 
 
 public class PrimaryController {
+
+    @FXML
+    private Button startButton;
 
     @FXML
     private void startButtonClicked(ActionEvent actionEvent) throws IOException{
