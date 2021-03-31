434091a713cd2c03a25839dddaf8bf447c6896c0
MoKhedr123 <80634894+MoKhedr123@users.noreply.github.com>
2021-03-31 11:58:49 +0200

    Add files via upload

---
+++ DB_survey5/src/main/java/mohdkhedr/db_survey5/Application.java
@@ -0,0 +1,51 @@
+package mohdkhedr.db_survey5;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Scanner;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import org.h2.tools.Server;
+
+public class Application {
+
+    public static void main(String[] args) throws Exception {
+        startDatabase();
+
+        /*Customer customer = new Customer();
+        customer.setFirstName("Dennys");
+        customer.setLastName("Fredericci");*/
+        
+        Data d = new Data();
+        d.setEmail("mohdkhedr123@gmail.com");
+        d.setName("Mohamed");
+        d.setUsername("momothebeast");
+        d.setPassword("123456");
+        
+        try(DataDAO dDAO = new JpaDataDAO();){
+            dDAO.saveData(d);
+            System.out.println("Please press a key to change your name too pranay");
+            (new Scanner(System.in)).nextLine();
+            d.setName("pranay");
+            dDAO.saveData(d);
+           List<Data> dList = dDAO.getData();
+            for (Data data : dList) {
+                data.setUsername("mo");
+               dDAO.saveData(d);
+            }
+              System.out.println(dList);
+        }
+       
+        
+        /*System.out.println("Open your browser and navigate to http://localhost:8082/");
+        System.out.println("JDBC URL: jdbc:h2:mem:my_database");
+        System.out.println("User Name: sa");
+        System.out.println("Password: ");*/
+
+    }
+
+    private static void startDatabase() throws SQLException {
+        new Server().runTool("-tcp", "-web", "-ifNotExists");
+    }
+}
