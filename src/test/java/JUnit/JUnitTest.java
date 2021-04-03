387d8cb9d6559264479cb17700bef78e8acc8420
Salma <salma.afifi5555@gmail.com>
2021-04-03 15:51:29 +0200

    Somechanges

---
+++ src/test/java/JUnit/JUnitTest.java
@@ -0,0 +1,49 @@
+package JUnit;
+
+
+import static org.junit.Assert.*;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+
+
+
+
+public class JUnitTest {
+    
+    public JUnitTest() {
+    }
+
+    @BeforeAll
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterAll
+    public static void tearDownClass() throws Exception {
+    }
+
+    @BeforeEach
+    public void setUp() throws Exception {
+    }
+
+    @AfterEach
+    public void tearDown() throws Exception {
+    }
+    
+    @Test
+    public void passTest(boolean True){
+        assertTrue(True);
+    }
+    
+    
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
