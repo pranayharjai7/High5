bf4ed80d68e79af1552b8ac31762684cdb6d82d1
mariasher001 <mariasher001@gmail.com>
2021-04-20 06:38:02 +0200

    Controller And Toggle Updated

--- src/main/java/Survey5/controller/SurveyTemplateControllers/HealthTemplate3Controller.java
+++ src/main/java/Survey5/controller/SurveyTemplateControllers/HealthTemplate3Controller.java
@@ -3,6 +3,10 @@ package Survey5.controller.SurveyTemplateControllers;
 import Survey5.MainApp;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
@@ -11,6 +15,156 @@ public class HealthTemplate3Controller
 {
     @FXML
     private AnchorPane HealthTemplate3AnchorPane;
+
+    @FXML
+    private Label Q1;
+
+    @FXML
+    private RadioButton Ans1_2;
+
+    @FXML
+    private ToggleGroup group1;
+
+    @FXML
+    private RadioButton Ans1_3;
+
+    @FXML
+    private RadioButton Ans1_4;
+
+    @FXML
+    private Label Q2;
+
+    @FXML
+    private RadioButton Ans2_1;
+
+    @FXML
+    private ToggleGroup group2;
+
+    @FXML
+    private RadioButton Ans2_2;
+
+    @FXML
+    private RadioButton Ans2_3;
+
+    @FXML
+    private RadioButton Ans2_4;
+
+    @FXML
+    private Label Q3;
+
+    @FXML
+    private RadioButton Ans3_1;
+
+    @FXML
+    private ToggleGroup group3;
+
+    @FXML
+    private RadioButton Ans3_2;
+
+    @FXML
+    private RadioButton Ans3_3;
+
+    @FXML
+    private RadioButton Ans3_4;
+
+    @FXML
+    private Button submitButtonClicked;
+
+    @FXML
+    private Label Q5;
+
+    @FXML
+    private RadioButton Ans4_1;
+
+    @FXML
+    private ToggleGroup group4;
+
+    @FXML
+    private RadioButton Ans4_2;
+
+    @FXML
+    private RadioButton Ans5_1;
+
+    @FXML
+    private ToggleGroup group5;
+
+    @FXML
+    private RadioButton Ans5_2;
+
+    @FXML
+    private Label Q6;
+
+    @FXML
+    private RadioButton Ans6_1;
+
+    @FXML
+    private ToggleGroup group6;
+
+    @FXML
+    private RadioButton Ans6_2;
+
+    @FXML
+    private Label Q7;
+
+    @FXML
+    private RadioButton Ans7_2;
+
+    @FXML
+    private ToggleGroup group7;
+
+    @FXML
+    private RadioButton Ans7_1;
+
+    @FXML
+    private Label Q8;
+
+    @FXML
+    private RadioButton Ans8_1;
+
+    @FXML
+    private ToggleGroup group8;
+
+    @FXML
+    private RadioButton Ans8_2;
+
+    @FXML
+    private Label Q9;
+
+    @FXML
+    private RadioButton Ans9_1;
+
+    @FXML
+    private ToggleGroup group9;
+
+    @FXML
+    private RadioButton Ans9_2;
+
+    @FXML
+    private Label Q10;
+
+    @FXML
+    private RadioButton Ans10_1;
+
+    @FXML
+    private ToggleGroup group10;
+
+    @FXML
+    private RadioButton Ans10_2;
+
+    @FXML
+    private Label HealthTemplate3;
+
+    @FXML
+    private RadioButton Ans1_1;
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    void SubmitButtonClicked(ActionEvent event) throws IOException {
+        MainApp.setRoot("/fxml/SurveyTemplates/TemplateSurvey.fxml");
+
+    }
 
     public void backButtonClicked(ActionEvent actionEvent) throws IOException {
         MainApp.setRoot("/fxml/SurveyTemplates/TemplateSurvey.fxml");
--- src/main/java/Survey5/controller/SurveyTemplateControllers/HealthTemplate4Controller.java
+++ src/main/java/Survey5/controller/SurveyTemplateControllers/HealthTemplate4Controller.java
@@ -3,13 +3,183 @@ package Survey5.controller.SurveyTemplateControllers;
 import Survey5.MainApp;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
 
 import java.io.IOException;
 
 public class HealthTemplate4Controller {
     @FXML
     private AnchorPane HealthTemplate4AnchorPane;
+
+    @FXML
+    private Label HealthTemplate4;
+
+    @FXML
+    private Label NameLAbel;
+
+    @FXML
+    private Label AgeLabel;
+
+    @FXML
+    private TextField NameLabelTextField;
+
+    @FXML
+    private ChoiceBox<?> AgeLabelTextField;
+
+    @FXML
+    private Label DrugLabel;
+
+    @FXML
+    private TextField DrugNameTextField;
+
+    @FXML
+    private Text Q1;
+
+    @FXML
+    private RadioButton Ans1_1;
+
+    @FXML
+    private ToggleGroup group1;
+
+    @FXML
+    private RadioButton Ans2_2;
+
+    @FXML
+    private Text Q2;
+
+    @FXML
+    private RadioButton Ans2_1;
+
+    @FXML
+    private ToggleGroup group2;
+
+    @FXML
+    private Text Q3;
+
+    @FXML
+    private RadioButton Ans3_1;
+
+    @FXML
+    private ToggleGroup group3;
+
+    @FXML
+    private RadioButton Ans3_2;
+
+    @FXML
+    private Text Q4;
+
+    @FXML
+    private RadioButton Ans4_1;
+
+    @FXML
+    private ToggleGroup group4;
+
+    @FXML
+    private RadioButton Ans4_2;
+
+    @FXML
+    private Text Q5;
+
+    @FXML
+    private RadioButton Ans5_1;
+
+    @FXML
+    private ToggleGroup group5;
+
+    @FXML
+    private RadioButton Ans5_2;
+
+    @FXML
+    private Text Q6;
+
+    @FXML
+    private RadioButton Ans6_1;
+
+    @FXML
+    private ToggleGroup group6;
+
+    @FXML
+    private RadioButton Ans6_2;
+
+    @FXML
+    private Text Q7;
+
+    @FXML
+    private RadioButton Ans7_1;
+
+    @FXML
+    private ToggleGroup group7;
+
+    @FXML
+    private RadioButton Ans7_2;
+
+    @FXML
+    private Text Q8;
+
+    @FXML
+    private RadioButton Ans8_1;
+
+    @FXML
+    private ToggleGroup group8;
+
+    @FXML
+    private RadioButton Ans8_2;
+
+    @FXML
+    private Text Q9;
+
+    @FXML
+    private RadioButton Ans9_1;
+
+    @FXML
+    private ToggleGroup group9;
+
+    @FXML
+    private RadioButton Ans9_2;
+
+    @FXML
+    private Text Q10;
+
+    @FXML
+    private RadioButton Ans10_1;
+
+    @FXML
+    private ToggleGroup group10;
+
+    @FXML
+    private RadioButton Ans10_2;
+
+    @FXML
+    private Text Q11;
+
+    @FXML
+    private RadioButton Ans11_1;
+
+    @FXML
+    private ToggleGroup group11;
+
+    @FXML
+    private RadioButton Ans11_2;
+
+    @FXML
+    private Text Q12;
+
+    @FXML
+    private TextArea Ans12;
+
+    @FXML
+    private Button SaveSurvey;
+
+    @FXML
+    private Button backButton;
+    @FXML
+    private Button SaveButton;
+
+    public void SaveButtonClicked(ActionEvent actionEvent) throws IOException {
+        MainApp.setRoot("/fxml/SurveyTemplates/TemplateSurvey.fxml");
+    }
 
     public void backButtonClicked(ActionEvent actionEvent) throws IOException {
         MainApp.setRoot("/fxml/SurveyTemplates/TemplateSurvey.fxml");
