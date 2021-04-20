01f37eb4f98d2512f21c29a70bedd006dd229d30
mariasher001 <mariasher001@gmail.com>
2021-04-20 04:48:34 +0200

    Controller_Updated
    Business Controller 4 Updated

--- src/main/java/Survey5/controller/SurveyTemplateControllers/BusinessTemplate4Controller.java
+++ src/main/java/Survey5/controller/SurveyTemplateControllers/BusinessTemplate4Controller.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 
@@ -15,23 +16,27 @@ public class BusinessTemplate4Controller
 {
     @FXML
     private AnchorPane BusinessTemplate4AnchorPane;
+
     @FXML
-    private Label BusinessTemplate4Label;
+    private Label BusinessTemplate4;
 
     @FXML
     private Text SurveyQ1;
 
     @FXML
-    private RadioButton SurveyQ1Option1;
+    private RadioButton Ans1_1;
 
     @FXML
-    private RadioButton SurveyQ1Option2;
+    private RadioButton Ans1_2;
 
     @FXML
-    private RadioButton SurveyQ1Option3;
+    private RadioButton Ans1_3;
 
     @FXML
-    private RadioButton SurveyQ1Option4;
+    private RadioButton Ans1_4;
+
+    @FXML
+    private TextField Ans1_5;
 
     @FXML
     private RadioButton SurveyQ1Option5TextField;
@@ -40,104 +45,115 @@ public class BusinessTemplate4Controller
     private Text SurveyQ2;
 
     @FXML
-    private RadioButton getSurveyQ1Option1;
+    private RadioButton Ans2_2;
+
+    @FXML
+    private RadioButton Ans2_1;
+
+    @FXML
+    private RadioButton Ans2_3;
+
+    @FXML
+    private RadioButton Ans2_4;
 
     @FXML
     private Text SurveyQ3;
 
     @FXML
-    private RadioButton SurveyQ3Option1;
+    private RadioButton Ans3_1;
 
     @FXML
-    private RadioButton SurveyQ3Option2;
+    private RadioButton Ans3_2;
 
     @FXML
-    private RadioButton SurveyQ3Option4;
+    private RadioButton Ans3_4;
 
     @FXML
-    private RadioButton SurveyQ3Option3;
+    private RadioButton Ans3_3;
 
     @FXML
     private Text SurveyQ4;
 
     @FXML
-    private RadioButton SurveyQ4Option2;
+    private RadioButton Ans4_2;
 
     @FXML
-    private RadioButton SurveyQ4Option1;
+    private RadioButton Ans4_1;
 
     @FXML
-    private RadioButton SurveyQ4Option3;
+    private RadioButton Ans4_3;
 
     @FXML
-    private RadioButton SurveyQ4Option4;
+    private RadioButton Ans4_4;
 
     @FXML
     private Text SurveyQ5;
 
     @FXML
-    private RadioButton SurveyQ5Option1;
+    private RadioButton Ans5_1;
 
     @FXML
-    private RadioButton SurveyQ5Option2;
+    private RadioButton Ans5_2;
 
     @FXML
-    private RadioButton SurveyQ5Option3;
+    private RadioButton Ans5_3;
 
     @FXML
-    private RadioButton SurveyQ5Option4;
+    private RadioButton Ans5_4;
 
     @FXML
     private Text SurveyQ6;
 
     @FXML
-    private RadioButton SurveyQ6Option1;
+    private RadioButton Ans6_1;
 
     @FXML
-    private RadioButton SurveyQ6Option2;
+    private RadioButton Ans6_2;
 
     @FXML
-    private RadioButton SurveyQ6Option3;
+    private RadioButton Ans6_3;
 
     @FXML
-    private RadioButton SurveyQ6Option4;
+    private RadioButton Ans6_4;
 
     @FXML
     private Text SurveyQ7;
 
     @FXML
-    private RadioButton SurveyQ7Option1;
+    private RadioButton Ans7_1;
 
     @FXML
-    private RadioButton SurveyQ7Option2;
+    private RadioButton Ans7_2;
 
     @FXML
-    private RadioButton SurveyQ7Option3;
+    private RadioButton Ans7_3;
 
     @FXML
-    private RadioButton SurveyQ7Option4;
+    private RadioButton Ans7_4;
 
     @FXML
     private Text Question8;
 
     @FXML
-    private RadioButton SurveyQ8Option1;
+    private RadioButton Ans8_1;
 
     @FXML
-    private RadioButton SurveyQ8Option2;
+    private RadioButton Ans8_2;
 
     @FXML
-    private RadioButton SurveyQ8Option3;
+    private RadioButton Ans8_3;
 
     @FXML
-    private RadioButton SurveyQ8Option4;
+    private RadioButton Ans8_4;
 
     @FXML
     private Button BusinessTemplate4SubmitButton;
 
     @FXML
-    void BusinessTemplate4SubmitButtonClicked(ActionEvent event) {
-
+    private Button backButton;
+    @FXML
+    void BusinessTemplate4SubmitButtonClicked(ActionEvent event) throws IOException {
+        MainApp.setRoot("/fxml/SurveyTemplates/TemplateSurvey.fxml");
     }
     public void backButtonClicked(ActionEvent actionEvent) throws IOException {
         MainApp.setRoot("/fxml/SurveyTemplates/TemplateSurvey.fxml");
